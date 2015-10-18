be58bb07fb402cc775d0fd2e9b5fd7edc94ee6ef
Mike Park <mikeparkms@gmail.com>
2015-10-18 04:52:38 -0700

    refactored voting code

--- voting.js
+++ voting.js
@@ -4,25 +4,53 @@ $(function() {
 	$('#submit-video').click(function() { submitVideo(); });
 
 	var submitVideo = function() {
+		$('.voting').empty();
+		generateVoteEntry();
 		var url = $('#video-url').val();
 		var id = url.split('?v=')[1].split('&')[0];
-		getVideoInfo(id);
+		getVideoInfo(id, updateVoteEntry);
 	}
 
-	var getVideoInfo = function(id) {
+	var generateVoteEntry = function() {
+		var domBlock =
+			'<div class="video-entry">' +
+				'<div class="vote-arrows">' +
+					'<i class="fa fa-thumbs-up"></i>' +
+					'<p id="counter">0</p>' +
+					'<i class="fa fa-thumbs-down"></i>' +
+				'</div>' +
+				'<div class="video-thumbnail"></div>' +
+				'<div class="video-info-text">' +
+					'<div class="video-title"></div>' +
+					'<div class="video-uploader"></div>' +
+				'</div>' +
+			'</div>';
+		$('.voting').append(domBlock);
+	}
+
+	var getVideoInfo = function(id, callback) {
 		var requestURL = 'https://www.googleapis.com/youtube/v3/videos?id=' + id + '&key=' + apiKey + '&part=snippet';
-		$.getJSON(requestURL, parseInfo);
+		$.getJSON(requestURL, function(data) {
+			var parsedInfo = parseInfo(data);
+			callback(parsedInfo);
+		});
 	}
 
 	var parseInfo = function(data) {
-		var title = data.items[0].snippet.title;
-		var uploader = data.items[0].snippet.channelTitle;
-		var thumbURL = data.items[0].snippet.thumbnails.default.url;
+		var res = {
+			'title': data.items[0].snippet.title,
+			'uploader': data.items[0].snippet.channelTitle,
+			'thumbURL': data.items[0].snippet.thumbnails.default.url
+		}
+		return res;
 		
-		$('.video-title').text(title);
-		$('.video-uploader').text(uploader);
+	}
+
+	var updateVoteEntry = function(data) {
+		$('.video-title').text(data.title);
+		$('.video-uploader').text(data.uploader);
 		$('.video-thumbnail').empty();
-		$('.video-thumbnail').append('<img src="' + thumbURL + '" width="90px">');
+		$('.video-thumbnail').append('<img src="' + data.thumbURL + '" width="90px">');
 	}
 });
 
