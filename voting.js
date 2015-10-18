9ca56fc975c84447cbe98e699a9b7a511f6bdcdf
Mike Park <mikeparkms@gmail.com>
2015-10-18 02:37:58 -0700

    Basic functionality for getting video info from YT; no communication with server yet

---
+++ voting.js
@@ -0,0 +1,26 @@
+$(function() {
+	var apiKey = 'AIzaSyACwwIf-fpEqbbjWd7Ae09je7ixYyJsqc4';
+
+	$('#submit-video').click(function() { submitVideo(); });
+
+	var submitVideo = function() {
+		var url = $('#video-url').val();
+		var id = url.split('?v=')[1].split('&')[0];
+		getVideoInfo(id);
+	}
+
+	var getVideoInfo = function(id) {
+		var requestURL = 'https://www.googleapis.com/youtube/v3/videos?id=' + id + '&key=' + apiKey + '&part=snippet';
+		$.getJSON(requestURL, parseInfo);
+	}
+
+	var parseInfo = function(data) {
+		var title = data.items[0].snippet.title;
+		var uploader = data.items[0].snippet.channelTitle;
+		var thumbURL = data.items[0].snippet.thumbnails.default.url;
+		
+		$('.video-title').text(title);
+		$('.video-uploader').text(uploader);
+		$('.video-thumbnail').append('<img src="' + thumbURL + '" width="90px">');
+	}
+});
