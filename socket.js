eb711591178d9732eec98f1cff2d264a62ba5d73
Mike Park <mikeparkms@gmail.com>
2015-10-18 05:57:51 -0700

    automatically update title

--- socket.js
+++ socket.js
@@ -12,6 +12,22 @@ socket.on('connect',function() {
   console.log('Client has connected!');
 });
 
+$(function() {
+	$('#submit-video').click(function() {
+		var toSend = {
+			'videoURL': $('#video-url').val();
+		}
+		socket.emit('video added', toSend);
+	});
+});
 
-
-
+socket.on('video list', function(data) {
+	var sorted = sortVideos(data);
+	var length = Object.keys(sorted).length;
+	$('.voting').empty();
+	// for (var i=0; i++; i<length) {
+	// 	generateVoteEntry();
+	// 	var id = sorted[i].split('?v=')[1].split('&')[0];
+		
+	// }
+})
