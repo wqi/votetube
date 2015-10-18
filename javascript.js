417247b2fffe86837683e8f4b5bd985d07e012ab
William Qi <william.qi@zenchatapp.com>
2015-10-18 04:34:46 -0700

    throttle on mouse move

--- javascript.js
+++ javascript.js
@@ -28,6 +28,28 @@
 			}
 		}
 
+		function throttle(fn, threshhold, scope) {
+		  threshhold || (threshhold = 250);
+		  var last,
+		      deferTimer;
+		  return function () {
+		    var context = scope || this;
+
+		    var now = +new Date,
+		        args = arguments;
+		    if (last && now < last + threshhold) {
+		      clearTimeout(deferTimer);
+		      deferTimer = setTimeout(function () {
+		        last = now;
+		        fn.apply(context, args);
+		      }, threshhold);
+		    } else {
+		      last = now;
+		      fn.apply(context, args);
+		    }
+		  };
+		}
+
 		function getCursorPosition(e){
 			// Mouse moves. Now show tabs
 			clearTimeout(hideTabs);
