a49ddf0591b737092dc5aa87c8c7386db4fdbe3b
Mike Park <mikeparkms@gmail.com>
2015-10-18 01:01:20 -0700

    Working on voting UI

--- UI/youtube.js
+++ UI/youtube.js
@@ -13,7 +13,7 @@ function onYouTubeIframeAPIReady() {
     height: window.innerHeight,
 	width: window.innerWidth,
 	videoId: 'Vb5OZKna62Q',
-    playerVars: {'wmode': 'opaque', 'autoplay': 1, 'controls': 0, 'showinfo': 0},
+    playerVars: {'wmode': 'opaque', 'autoplay': 0, 'controls': 0, 'showinfo': 0},
     events: {
       'onReady': onPlayerReady,
     }
@@ -22,5 +22,5 @@ function onYouTubeIframeAPIReady() {
 
 // 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-  event.target.playVideo();
+  // event.target.playVideo();
 }
